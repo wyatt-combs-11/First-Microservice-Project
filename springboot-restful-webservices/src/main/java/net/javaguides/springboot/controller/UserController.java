5cf1bbcf953afa01f2e0aa3201fb69f120267a47
Combs <wcombs@cvg-000216s-569.local>
2023-06-01 14:24:05 -0400

    Added Sections 3/4: CRUD operations and DTO

---
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,60 @@
+package net.javaguides.springboot.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    // build create User REST API
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
+        UserDto savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    // build get user by id REST API
+    // http://localhost:8080/api/users/1
+    @GetMapping("{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
+        UserDto user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // build get all users REST API
+    // http://localhost:8080/api/users
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // build update user REST API
+    // http://localhost:8080/api/users/1
+    @PutMapping("{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,
+                                           @RequestBody UserDto user) {
+        user.setId(userId);
+        UserDto updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    // build delete user REST API
+    // http://localhost:8080/api/users/1
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User successfully deleted!", HttpStatus.OK);
+    }
+}
---
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package net.javaguides.springboot.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.mapper.UserMapper;
+import net.javaguides.springboot.repository.UserRepository;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        // Convert UserDto into JPA Entity
+        User user = UserMapper.mapToUser(userDto);
+
+        User savedUser = userRepository.save(user);
+
+        // Convert User JPA Entity to UserDto
+        UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+
+        return savedUserDto;
+    }
+
+    @Override
+    public UserDto getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return UserMapper.mapToUserDto(optionalUser.get());
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+       List<User> users = userRepository.findAll();
+       return users.stream().map(UserMapper::mapToUserDto)
+               .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto updateUser(UserDto user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = userRepository.save(existingUser);
+        return UserMapper.mapToUserDto(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
